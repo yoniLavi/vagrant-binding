67df61f62e9bd84c532523fd2437a385a09fc13b
Yoni Yalovitsky <yoni@fewbytes.com>
2013-07-01 16:11:58 +0300

    added basic json output

--- src/main/java/CloudDriverPOC.java
+++ src/main/java/CloudDriverPOC.java
@@ -14,86 +14,103 @@ import com.guigarage.vagrant.configuration.builder.VagrantEnvironmentConfigBuild
 import com.guigarage.vagrant.configuration.builder.VagrantVmConfigBuilder;
 import com.guigarage.vagrant.model.VagrantEnvironment;
 import com.guigarage.vagrant.model.VagrantVm;
+import org.json.JSONWriter;
+import org.json.JSONException;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import static spark.Spark.*;
 
+
 public class CloudDriverPOC {
 
-    public static void startSparc(final VagrantEnvironment environment) {
-        setIpAddress("127.0.0.1");
-        setPort(9101);
+	public static void startSparc(final VagrantEnvironment environment) {
+		setIpAddress("127.0.0.1");
+		setPort(9101);
 
-        get(new Route("/instance") {
-            @Override
-            public Object handle(Request request, Response response) {
-                response.type("text/html");
-                String output = "These are your vms:\n";
-                for (VagrantVm vm : environment.getAllVms()) {
-                    output += String.format("* %s is %s\n", vm.getName(), vm.isRunning() ? "up" : "down");
-                }
-                return output;
-            }
-        });
+		get(new Route("/instance") {
+			@Override
+			public Object handle(Request request, Response response) {
+				response.type("application/json");
+				StringWriter output = new StringWriter();
+				try {
+					JSONWriter json = new JSONWriter(output).array();
+					for (VagrantVm vm : environment.getAllVms()) {
+						json.object();
+						json.key("name").value(vm.getName());
+						json.key("status").value(vm.isRunning() ? "up" : "down");
+						json.endObject();
+					}
+					json.endArray();
+				} catch (JSONException e){
+					//just return whatever we have so far
+					response.status(503);
+				}
 
-        post(new Route("/instance") {
-            @Override
-            public Object handle(Request request, Response response) {
-                response.type("text/html");
-                environment.up();
-                return "Successfully launched your environment";
-            }
-        });
+				return output.toString();
+			}
+		});
 
-        delete(new Route("/instance") {
-            @Override
-            public Object handle(Request request, Response response) {
-                response.type("text/html");
-                environment.destroy();
-                return "Environment destroyed";
-            }
-        });
-    }
+		post(new Route("/instance") {
+			@Override
+			public Object handle(Request request, Response response) {
+				response.type("text/html");
+				environment.up();
+				return "Successfully launched your environment";
+			}
+		});
 
-    public static VagrantEnvironment createEnvironment(String VMName, String ip, String envPath) throws IOException {
-        VagrantVmConfig vmConfig = VagrantVmConfigBuilder
-                                           .create()
-                                           .withLucid32Box()
-                                           .withName(VMName)
-                                           .withHostOnlyIp(ip)
-                                           .withVagrantPortForwarding(new VagrantPortForwarding(22, 8022))
-                                           .build();
+		delete(new Route("/instance") {
+			@Override
+			public Object handle(Request request, Response response) {
+				response.type("text/html");
+				environment.destroy();
+				return "Environment destroyed";
+			}
+		});
+	}
 
-        VagrantEnvironmentConfig environmentConfig = VagrantEnvironmentConfigBuilder
-                                                             .create()
-                                                             .withVagrantVmConfig(vmConfig)
-                                                             .build();
+	public static VagrantEnvironment createEnvironment(String BoxName, String VMName, String ip, String envPath)
+			throws IOException {
 
-        Vagrant vagrant = new Vagrant(true);
+		VagrantVmConfig vmConfig = VagrantVmConfigBuilder
+										.create()
+										.withBoxName(BoxName)
+										.withName(VMName)
+										.withHostOnlyIp(ip)
+										.withVagrantPortForwarding(new VagrantPortForwarding(22, 8022))
+										.build();
 
-        return vagrant.createEnvironment(new File(envPath), environmentConfig);
-    }
+		VagrantEnvironmentConfig environmentConfig = VagrantEnvironmentConfigBuilder
+				                                             .create()
+				                                             .withVagrantVmConfig(vmConfig)
+				                                             .build();
 
-    public static void main(String[] args) {
-        VagrantEnvironment environment = null;
+		Vagrant vagrant = new Vagrant(true);
 
-        try {
-            environment = createEnvironment("myVM", "192.168.0.101", "/tmp/vagrantEnv");
+		return vagrant.createEnvironment(new File(envPath), environmentConfig);
+	}
 
-            startSparc(environment);
+	public static void main(String[] args) {
+		VagrantEnvironment environment = null;
 
-        } catch (IOException e) {
-            e.printStackTrace();
+		try {
+			environment = createEnvironment("lucid32", "myVM", "192.168.0.101", "/tmp/vagrantEnv");
+
+			startSparc(environment);
+
+		} catch (IOException e) {
+			e.printStackTrace();
 //        } finally {
 //            if (environment != null) {
 //                environment.destroy();
 //            }
-        }
+		}
 
-    }
+	}
 }
